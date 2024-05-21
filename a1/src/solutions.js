44eefedb87a175fc6f6250f1b78edfdcabd13df1
Luca Novello <gnovello@myseneca.ca>
2024-05-21 12:05:18 -0400

    updated assignment 1

--- a1/src/solutions.js
+++ a1/src/solutions.js
@@ -138,15 +138,16 @@ function greeting(name) {
  ******************************************************************************/
 
 function toCamelCase(name, uppercase) {
-  const nameArr = name.split('-');
-  for (let i = 1; i < nameArr.length; i++) {
+  const startIndex = uppercase ? 0 : 1;
+  const lowerName = name.toLowerCase();
+  const nameArr = lowerName.split('-');
+  for (let i = startIndex; i < nameArr.length; i++) {
     // Capitalize the first letter and lowercase the rest
     nameArr[i] = nameArr[i].charAt(0).toUpperCase() + nameArr[i].slice(1).toLowerCase();
   }
   // Join the names back into a single string
   let newName = nameArr.join('');
   // If uppercase is true, capitalize the first letter of newName
-  uppercase && (newName = newName.charAt(0).toUpperCase() + newName.slice(1));
   return newName;
 }
 
@@ -202,7 +203,22 @@ function toCamelCase(name, uppercase) {
  ******************************************************************************/
 
 function createLinkTag(rel, href) {
-  // Replace this comment with your code...
+  console.log(
+    '####################################################################################################################################################################################################################################################'
+  );
+  console.log(rel, href);
+  const newRel = rel.trim();
+  const newHref = href.trim();
+  const reExp = /[\s\t\n\r]/;
+  console.log(newRel, newHref);
+
+  const relNeedsTest = reExp.test(newRel);
+  const hrefNeedsTest = reExp.test(newHref);
+  const finalRel = relNeedsTest ? `"${newRel}"` : `${newRel.replace(/'"/, '')}`;
+  const finalHref = hrefNeedsTest ? `"${newHref}"` : `${newHref.replace(/'"/, '')}`;
+  console.log(finalRel, finalHref);
+
+  return `<link rel=${finalRel} href=${finalHref}>`;
 }
 
 /*******************************************************************************
