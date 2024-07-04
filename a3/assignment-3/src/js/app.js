753864e9a1db537dbd5bcc392ca594d878e259c2
Luca Novello <gnovello@myseneca.ca>
2024-07-04 15:46:49 -0400

    added Assignment 3

---
+++ a3/assignment-3/src/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * WEB222 – Assignment 3
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Luca Novello
+ *      Student ID: 038515003
+ *      Date:       July 2, 2024
+ */
+
+// All of our data is available on the global `window` object.
+// Create local variables to work with it in this file.
+const { artists, songs } = window;
+
+// For debugging, display all of our data in the console. You can remove this later.
+console.log({ artists, songs }, "Song and Artist App Data");
