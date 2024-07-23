c06ff852559b9340b07a18697330f63287fc6351
Luca Novello <gnovello@myseneca.ca>
2024-07-23 15:04:08 -0400

    added a4

---
+++ a4/music-app/src/js/songs.js
@@ -0,0 +1,221 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  {
+    songId: "SID-001",
+    artistId: "AID-001",
+    title: "Stayin' Alive",
+    released: 1977,
+    duration: 238,
+    mediaUrl: "https://www.youtube.com/watch?v=I_izvAbhExY",
+    explicitLyrics: false,
+    imageUrl:
+      "https://static01.nyt.com/images/2020/12/14/arts/14-BEEGEES-DOC-1/merlin_180811818_23168181-418e-4088-9c16-0f4e2f2b2324-superJumbo.jpg?quality=75&auto=webp",
+    imagePosition: "50% 40%"
+  },
+  {
+    songId: "SID-002",
+    artistId: "AID-001",
+    title: "Night Fever",
+    released: 1977,
+    duration: 193,
+    mediaUrl: "https://www.youtube.com/watch?v=SkypZuY6ZvA",
+    explicitLyrics: false,
+    imageUrl:
+      "https://static01.nyt.com/images/2020/12/14/arts/14-BEEGEES-DOC-1/merlin_180811818_23168181-418e-4088-9c16-0f4e2f2b2324-superJumbo.jpg?quality=75&auto=webp",
+    imagePosition: "50% 40%"
+  },
+  {
+    songId: "SID-003",
+    artistId: "AID-001",
+    title: "You Should Be Dancing",
+    released: 1976,
+    duration: 259,
+    mediaUrl: "https://www.youtube.com/watch?v=1sqE6P3XyiQ",
+    explicitLyrics: false,
+    imageUrl:
+      "https://static01.nyt.com/images/2020/12/14/arts/14-BEEGEES-DOC-1/merlin_180811818_23168181-418e-4088-9c16-0f4e2f2b2324-superJumbo.jpg?quality=75&auto=webp",
+    imagePosition: "50% 40%"
+  },
+  {
+    songId: "SID-004",
+    artistId: "AID-002",
+    title: "Hot Stuff",
+    released: 1979,
+    duration: 276,
+    mediaUrl: "https://www.youtube.com/watch?v=1IdEhvuNxV8",
+    explicitLyrics: false,
+    imageUrl:
+      "https://www.udiscovermusic.com/wp-content/uploads/2022/12/Donna-Summer-GettyImages-548231291-1000x600.jpg",
+    imagePosition: "0% 15%"
+  },
+  {
+    songId: "SID-005",
+    artistId: "AID-002",
+    title: "Bad Girls",
+    released: 1979,
+    duration: 252,
+    mediaUrl: "https://www.youtube.com/watch?v=0F_CkskgUIA",
+    explicitLyrics: true,
+    imageUrl:
+      "https://www.udiscovermusic.com/wp-content/uploads/2022/12/Donna-Summer-GettyImages-548231291-1000x600.jpg",
+    imagePosition: "0% 15%"
+  },
+  {
+    songId: "SID-006",
+    artistId: "AID-002",
+    title: "I Feel Love",
+    released: 1977,
+    duration: 237,
+    mediaUrl: "https://www.youtube.com/watch?v=Nm-ISatLDG0",
+    explicitLyrics: false,
+    imageUrl:
+      "https://www.udiscovermusic.com/wp-content/uploads/2022/12/Donna-Summer-GettyImages-548231291-1000x600.jpg",
+    imagePosition: "0% 15%"
+  },
+  {
+    songId: "SID-007",
+    artistId: "AID-003",
+    title: "That's the Way (I Like It)",
+    released: 1975,
+    duration: 212,
+    mediaUrl: "https://www.youtube.com/watch?v=yupPvZF6oBw",
+    explicitLyrics: false,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BNzU1ODYwOTUtYmEwZS00ZWMyLTgyZjctZGZjZmQzMWNhYWMyXkEyXkFqcGdeQXVyMjUyNDk2ODc@._V1_.jpg",
+    imagePosition: "0% 12%"
+  },
+  {
+    songId: "SID-008",
+    artistId: "AID-003",
+    title: "Get Down Tonight",
+    released: 1975,
+    duration: 204,
+    mediaUrl: "https://www.youtube.com/watch?v=BXIBEW5MLuU",
+    explicitLyrics: false,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BNzU1ODYwOTUtYmEwZS00ZWMyLTgyZjctZGZjZmQzMWNhYWMyXkEyXkFqcGdeQXVyMjUyNDk2ODc@._V1_.jpg",
+    imagePosition: "0% 12%"
+  },
+  {
+    songId: "SID-009",
+    artistId: "AID-003",
+    title: "Boogie Shoes",
+    released: 1975,
+    duration: 158,
+    mediaUrl: "https://www.youtube.com/watch?v=Ia0zeuZMJbo",
+    explicitLyrics: false,
+    imageUrl:
+      "https://m.media-amazon.com/images/M/MV5BNzU1ODYwOTUtYmEwZS00ZWMyLTgyZjctZGZjZmQzMWNhYWMyXkEyXkFqcGdeQXVyMjUyNDk2ODc@._V1_.jpg",
+    imagePosition: "0% 12%"
+  },
+  {
+    songId: "SID-010",
+    artistId: "AID-004",
+    title: "Dancing Queen",
+    released: 1976,
+    duration: 231,
+    mediaUrl: "https://www.youtube.com/watch?v=xFrGuyw1V8s",
+    explicitLyrics: false,
+    imageUrl:
+      "https://people.com/thmb/HHAwUeUtJN7vc2YMZ-X9FMHnV1o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x520:1001x522)/abba-2-7f1f4e6d1c1644da8f08c078bfd6c886.jpg",
+    imagePosition: "0% 15%"
+  },
+  {
+    songId: "SID-011",
+    artistId: "AID-004",
+    title: "Mamma Mia",
+    released: 1975,
+    duration: 207,
+    mediaUrl: "https://www.youtube.com/watch?v=unfzfe8f9NI",
+    explicitLyrics: false,
+    imageUrl:
+      "https://people.com/thmb/HHAwUeUtJN7vc2YMZ-X9FMHnV1o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x520:1001x522)/abba-2-7f1f4e6d1c1644da8f08c078bfd6c886.jpg",
+    imagePosition: "0% 15%"
+  },
+  {
+    songId: "SID-012",
+    artistId: "AID-004",
+    title: "Gimme! Gimme! Gimme! (A Man After Midnight)",
+    released: 1979,
+    duration: 293,
+    mediaUrl: "https://www.youtube.com/watch?v=XEjLoHdbVeE",
+    explicitLyrics: false,
+    imageUrl:
+      "https://people.com/thmb/HHAwUeUtJN7vc2YMZ-X9FMHnV1o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x520:1001x522)/abba-2-7f1f4e6d1c1644da8f08c078bfd6c886.jpg",
+    imagePosition: "0% 15%"
+  },
+  {
+    songId: "SID-013",
+    artistId: "AID-005",
+    title: "I Will Survive",
+    released: 1978,
+    duration: 198,
+    mediaUrl: "https://www.youtube.com/watch?v=6dYWe1c3OyU",
+    explicitLyrics: false,
+    imageUrl:
+      "https://www.gloriagaynor.com/wp-content/themes/gloriagaynor2017/images/bg-home-performances.jpg",
+    imagePosition: "0% 0%"
+  },
+  {
+    songId: "SID-014",
+    artistId: "AID-005",
+    title: "Never Can Say Goodbye",
+    released: 1974,
+    duration: 189,
+    mediaUrl: "https://www.youtube.com/watch?v=vQn4fFd8p-8",
+    explicitLyrics: false,
+    imageUrl:
+      "https://www.gloriagaynor.com/wp-content/themes/gloriagaynor2017/images/bg-home-performances.jpg",
+    imagePosition: "0% 0%"
+  },
+  {
+    songId: "SID-015",
+    artistId: "AID-005",
+    title: "Reach Out, I'll Be There",
+    released: 1975,
+    duration: 200,
+    mediaUrl: "https://www.youtube.com/watch?v=cbcFdxyV6I4",
+    explicitLyrics: false,
+    imageUrl:
+      "https://www.gloriagaynor.com/wp-content/themes/gloriagaynor2017/images/bg-home-performances.jpg",
+    imagePosition: "0% 0%"
+  },
+  {
+    songId: "SID-016",
+    artistId: "AID-006",
+    title: "Disco Inferno",
+    released: 1976,
+    duration: 311,
+    mediaUrl: "https://www.youtube.com/watch?v=cWqcgPmCbDQ",
+    explicitLyrics: false,
+    imageUrl: "https://ychef.files.bbci.co.uk/1280x720/p08mqhyl.jpg",
+    imagePosition: "0% 10%"
+  },
+  {
+    songId: "SID-017",
+    artistId: "AID-006",
+    title: "Hold Back the Night",
+    released: 1975,
+    duration: 198,
+    mediaUrl: "https://www.youtube.com/watch?v=OGKgR4jPlyM",
+    explicitLyrics: false,
+    imageUrl: "https://ychef.files.bbci.co.uk/1280x720/p08mqhyl.jpg",
+    imagePosition: "0% 10%"
+  },
+  {
+    songId: "SID-018",
+    artistId: "AID-006",
+    title: "That's Where the Happy People Go",
+    released: 1976,
+    duration: 216,
+    mediaUrl: "https://www.youtube.com/watch?v=sgvQ9df5MPw",
+    explicitLyrics: false,
+    imageUrl: "https://ychef.files.bbci.co.uk/1280x720/p08mqhyl.jpg",
+    imagePosition: "0% 10%"
+  }
+];
